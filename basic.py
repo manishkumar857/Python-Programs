e95a784b910f2ad3e330530632276b8b958f0b45
Manish Kumar <jangramanish2602@gmail.com>
2024-07-31 22:34:46 +0530

    basic python programs

---
+++ basic.py
@@ -0,0 +1,49 @@
+"""a = int(input("enter first number"))
+b = int(input("enter second number"))
+print("sum ",a+b)"""
+
+"""name = input("enter name ")
+age = int(input("enter age "))
+cgpa = float(input("enter cgpa "))
+print("name:",name," age:",age," cgpa:",cgpa)"""
+
+'''length = int(input("enter length "))
+breadth =int(input("enter breadth "))  
+print("area is",length*breadth)
+b = length+breadth 
+print("perimeter is",2*b)'''
+
+'''tempINc = int(input("Enter temp in C ")) 
+F = (9/5)*tempINc+32
+print("Temp in farenheit is ",F,end=' ') 
+print("for temp in C ",tempINc)
+#end =' ' is used for removing next line 
+
+print("Temp in farenheit is ",F,end='@')
+print("for temp in C ",tempINc)
+
+print("twinkle twinkle little star\n\thow i wonder what you are\n\t\tup above the world so high\nlike a diamond in the sky")'''
+
+"""x=int(input("enter a number"))
+if(x<0):
+    print("number is negative")
+else:
+    if(x%2==0):  
+        print("number is even")
+    else:
+        print("number is odd")
+print("program ended successfully")"""
+
+
+x=int(input("enter 1st "))
+y=int(input("enter 2nd "))
+z=int(input("enter 3rd   "))
+
+if(x>y and x>z):
+    print("greatest number is ",x)
+elif(y>x and y>z):
+    print("greatest number is ",y)
+else:
+    print("greatest number is ",z)
+
+  
