bbce61feec8c2ea969a6ccb6b5197eca84b16efd
Manish <jangramanish2602@gmail.com>
2024-08-06 11:20:46 +0530

    conditional works for nonzero

---
+++ conditional.py
@@ -0,0 +1,45 @@
+'''x=int(input("enter a number"))
+if(x<0):
+    print("number is negative")
+else:
+    if(x%2==0):  
+        print("number is even")
+    else:
+        print("number is odd")
+print("program ended successfully")
+
+
+x=int(input("enter 1st "))
+y=int(input("enter 2nd "))
+z=int(input("enter 3rd   "))
+
+if(x>y and x>z):
+    print("greatest number is ",x)
+elif(y>x and y>z):
+    print("greatest number is ",y)
+else:
+    print("greatest number is ",z)'''
+
+#if will works for ALL non zero value
+
+if(1):
+    print("sucessfull")
+else:
+    print("unsucessfull")
+
+if(8):
+    print("sucessfull")
+else:
+    print("unsucessfull")
+
+if(0):
+    print("sucessfull")
+else:
+    print("unsucessfull")
+
+x=None
+
+if(x):
+    print("sucess")
+else:
+    print("unsucess")
