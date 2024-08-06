39c4e8a14c8e6f1f7102a1dcb16e5fef33110b19
Manish <jangramanish2602@gmail.com>
2024-08-06 11:07:14 +0530

    different type of print statements

--- First.py
+++ First.py
@@ -1,1 +1,13 @@
-#changes
+#other type of print statement
+a=10
+b=20
+
+print("sum of ",a,"is",b)
+
+print(f"sum of {a} is {b}")
+
+print("sum of "+str(a)+" is "+str(b))
+
+print("sum of %d is %d"%(a,b))
+
+print("sum of {} is {}".format(a,b))
