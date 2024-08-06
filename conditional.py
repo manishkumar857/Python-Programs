04958730b2ab899802ebaddf8c4f4745806a645f
Manish <jangramanish2602@gmail.com>
2024-08-06 15:27:01 +0530

    vote problem

--- conditional.py
+++ conditional.py
@@ -46,9 +46,9 @@ if(x):
 else:
     print("unsucessfull")'''
 
-x = int(input("Enter 1st number:"))
+'''x = int(input("Enter 1st number:"))
 y = int(input("Enter 2nd number:"))
-z = int(input("Enter 3rd number:"))
+z = int(input("Enter 3rd number:"))'''
 
 '''if(x>y and x>z):
     print(x,"is greatest")
@@ -61,11 +61,17 @@ elif(x==y and y==z and z==x):
 elif(x==y or y==z or z==x ):
     print("any two number are equal")'''
 
-if((x>y and x<z) or (x>z and x<y)):
+'''if((x>y and x<z) or (x>z and x<y)):
     print(x, "is the middle number")
  
 elif((y>x and y<z) or (y>z and y<x)):
     print(y,"is middle number")
+else:
+    print(z,"is middle number")'''
 
-elif((z>y and z<x) or (z<y and z>x)):
-    print(z,"is middle number")
+age = int(input("enter age : "))
+if(age>=18):
+    print("eligble to vote")
+else:
+    eli = 18-age
+    print("not eligible to vote can vote in ",eli," years")
