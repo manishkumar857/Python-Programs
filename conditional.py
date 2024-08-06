72b8aacf5a69d3aa4fd559f993c21a0d71503a72
Manish <jangramanish2602@gmail.com>
2024-08-06 14:37:56 +0530

    middle number

--- conditional.py
+++ conditional.py
@@ -50,9 +50,22 @@ x = int(input("Enter 1st number:"))
 y = int(input("Enter 2nd number:"))
 z = int(input("Enter 3rd number:"))
 
-if(x>y and x>z):
+'''if(x>y and x>z):
     print(x,"is greatest")
 elif(y>x and y>z):
     print(y,"is greatest")
 elif(z>y and z>y):
     print(z,"is greatest")
+elif(x==y and y==z and z==x):
+    print("all number are equal")
+elif(x==y or y==z or z==x ):
+    print("any two number are equal")'''
+
+if((x>y and x<z) or (x>z and x<y)):
+    print(x, "is the middle number")
+ 
+elif((y>x and y<z) or (y>z and y<x)):
+    print(y,"is middle number")
+
+elif((z>y and z<x) or (z<y and z>x)):
+    print(z,"is middle number")
