b1f5efe2da68fdebd6fbbb07c36083bac72867f5
Manish <jangramanish2602@gmail.com>
2024-08-09 14:53:30 +0530

    basic changes

--- First.py
+++ First.py
@@ -6,8 +6,8 @@ print("sum of ",a,"is",b)
 
 print(f"sum of {a} is {b}")
 
-print("sum of "+str(a)+" is "+str(b))
+print("sum of "+str(a)+" n "+str(b)+" is "+str(a+b))
 
-print("sum of %d is %d"%(a,b))
+print("sum of %d n %d is %d"%(a,b,a+b))
 
-print("sum of {} is {}".format(a,b))
+print("sum of {} n {} is {}".format(a,b,a+b))
--- basic.py
+++ basic.py
@@ -1,9 +1,9 @@
-"""
-#by default input is string type so we need to specify about the int type
+
+'''#by default input is string type so we need to specify about the int type
 
 a = int(input("enter first number"))
 b = int(input("enter second number"))
-print("sum ",a+b)"""
+print("sum ",a+b)'''
 
 """name = input("enter name ")
 age = int(input("enter age "))
@@ -29,11 +29,29 @@ print("twinkle twinkle little star\n\thow i wonder what you are\n\t\tup above th
 
 
 
-bill = int(input("enter amount "))
+'''bill = int(input("enter amount "))
 
 if(bill>1000):
     discount=bill/10
     bill = bill-discount
     print("final bill amount is ",bill," with a discount of ",discount)
 else:
-    print("final bill is ",bill)
+    print("final bill is ",bill)'''
+
+num =100
+num2='100'
+num3=100.9
+print(type(num))
+print(type(num2))
+print(type(num3))
+
+n1,n2,n3 =100,200,300
+print(n1,n2,n3)
+
+n1=n2=n3 =100
+print(n1,n2,n3) 
+
+n4=9j
+n5=True
+print(type(n4))
+print(type(n5))
