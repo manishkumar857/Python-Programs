8a2e5c973a914eec9731f3752e2f9683dd05b18a
Manish <jangramanish2602@gmail.com>
2024-08-13 10:43:33 +0530

    optimised way of lower upper prob

--- loops.py
+++ loops.py
@@ -1,8 +1,21 @@
-a1=int(input("enter number"))
-a2=int(input("enter number"))
+a1=int(input("enter lower number"))
+a2=int(input("enter upper number"))
 
-while(a1!=a2):
+'''while(a1<=a2):
     if(a1%2==0):
         print(a1)
+    a1=a1+1'''
+
+#optimised way
+
+if(a1%2==0):
+
+    a1=a1
+else:
     a1=a1+1
-    
+
+while(a1<=a2):
+    print(a1,end=" ")
+    a1=a1+2
+print("\nend of code")
+
