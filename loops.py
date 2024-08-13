4450bc215d17ccccc66b56d108386dc7ad6ac71b
Manish <jangramanish2602@gmail.com>
2024-08-13 11:33:37 +0530

    prime number problem

--- loops.py
+++ loops.py
@@ -1,10 +1,10 @@
-a1=int(input("enter lower number"))
+'''a1=int(input("enter lower number"))
 a2=int(input("enter upper number"))
 
-'''while(a1<=a2):
+while(a1<=a2):
     if(a1%2==0):
         print(a1)
-    a1=a1+1'''
+    a1=a1+1
 
 #optimised way
 
@@ -17,5 +17,23 @@ else:
 while(a1<=a2):
     print(a1,end=" ")
     a1=a1+2
-print("\nend of code")
+print("\nend of code")'''
+
+#prime number 
+a1=int(input("enter the number"))
+
+if(a1==0 or a1==1):
+    print("it's not a prime number")
+else:
+    i=2
+    k=0
+    while(i<a1):
+        if(a1%i==0):
+            k=k+1
+            break
+        i=i+1
+    if(k==0):
+        print("prime")
+    else:
+        print("not prime")
 
