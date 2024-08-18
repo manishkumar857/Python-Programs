7be03bab31008b3be13f6ca062db9c16c98f3bb6
Manish <jangramanish2602@gmail.com>
2024-08-18 10:00:00 +0530

    update lecture

--- new.py
+++ new.py
@@ -1,17 +1,30 @@
 #Membership->in,not in
-'''num1 =[10,20,30,40,50,100]
+num1= [10,20,30,40,50,100]
 
-ele=int(input("enter number to search"))
-print(ele in num1) #False
-print(ele not in num1)#True'''
+ele=int(input("Enter the number to search:"))
+print(ele in num1)#false
+print(ele not in num1)#false'''
 
-num1= int(input("enter first number :"))
-num2=int(input("enter second number :"))
+'''num1= int(input("Enter first number:"))
+num2= int(input("Enter second number:"))
 
 print(num1 & num2)
 print(num1 | num2)
 print(num1 ^ num2)
 print(~num1)
-print(num1 <<1)
-print(num2 >>1)
+print(num1<<1)
+print(num1>>1)'''
 
+'''num1= int(input("Enter first number:"))
+num2= int(input("Enter second number:"))
+num3= int(input("Enter third number:"))
+
+
+
+if (num1 > num2 and num1>num2):
+    print(num1,"is greatest")
+
+elif(num2>num1 and num2>num3):
+    print(num2,"is greater") 
+elif(num3>num1 and num3>num2):
+    print(num3, "is greater")'''
