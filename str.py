042d0203b8e861253750986aa38512868fd0f947
Manish <jangramanish2602@gmail.com>
2024-08-27 11:42:05 +0530

    reversing a string

--- str.py
+++ str.py
@@ -1,5 +1,5 @@
 str = "welcome to python "
-for i in str:
+'''for i in str:
     print(i,end="")
 
 
@@ -7,4 +7,11 @@ print(end="\n")
 i=0
 while(i<len(str)):
     print(str[i],end="")
-    i=i+1
+    i=i+1'''
+
+
+#reversing a string
+i=len(str)-1
+while(i>=0):
+    print(str[i],end="")
+    i=i-1
