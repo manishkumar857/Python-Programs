ff37a124333920a807a47d1bf3f76b8f785d037d
Manish <jangramanish2602@gmail.com>
2024-09-10 10:41:55 +0530

    slicing in list

--- List.py
+++ List.py
@@ -1,14 +1,20 @@
-ls = [2,2,4,5,5,6] # list are mutable , it allows duplicates values
-print(ls)
+# ls = [2,2,4,5,5,6] # list are mutable , it allows duplicates values
+# print(ls)
 
-ls.append(2) #can add only one argument
-print(ls)
-ls.append(2)
+# ls.append(2) #can add only one argument
+# print(ls)
+# ls.append(2)
 
-ls2=[3,3,3,3,3,3]
-ls.extend(ls2)
-print(ls)
+# ls2=[3,3,3,3,3,3]
+# ls.extend(ls2)
+# print(ls)
 
-ls.insert(0,76) #index,value
-print(ls)
+# ls.insert(0,76) #index,value
+# print(ls)
 
+#slicing in list
+ls=[6,7,8,9,3]
+print(ls[::1])#[start:end:stride(space in between])
+print(ls[1::1])
+print(ls[::2])
+print(ls[::-1])
