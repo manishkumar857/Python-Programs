34c89e5048ebca0449fc6f221c3f3304273ed26f
Manish <jangramanish2602@gmail.com>
2024-09-10 11:13:14 +0530

    n natural number list

--- List.py
+++ List.py
@@ -13,19 +13,32 @@
 # print(ls)
 
 #slicing in list
-ls=[6,7,8,9,3]
-print(ls[0])
-print(ls[::1])#[start:end:stride(space in between])
-print(ls[1::1])
-print(ls[::2])
-print(ls[::-1])
+# ls=[6,7,8,9,3]
+# print(ls[0])
+# print(ls[::1])#[start:end:stride(space in between])
+# print(ls[1::1])
+# print(ls[::2])
+# print(ls[::-1])
 
-print(ls)
-print(ls.pop(0)) #   pop() remove last element ,pop(inex value) remove index value
-print(ls)
+# print(ls)
+# print(ls.pop(0)) #   pop() remove last element ,pop(inex value) remove index value
+# print(ls)
 
-print(ls.remove(9)) # element name in ()
-print(ls)
+# print(ls.remove(9)) # element name in ()
+# print(ls)
 
-del ls[0:3]
-print(ls)
+# del ls[0:3]
+# print(ls)
+
+
+n =int(input("enter the no of elements till you want to print"))
+i=0
+x=0
+ls1 =[]
+while(x<=n):
+    
+    ls1.append(x)
+    i=i+1
+    x=i**3   
+print(ls1)
+
