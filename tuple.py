e1d4aadd7e28f78ddb14025f60ea7047dacfb96b
Manish <jangramanish2602@gmail.com>
2024-09-17 14:59:41 +0530

    tuple concepts

--- tuple.py
+++ tuple.py
@@ -6,7 +6,25 @@ print(CAmarks[-1])
 print(CAmarks[0:3])
 print(CAmarks[-3:])
 
-idx=int(input("enter the index : "))
-inp = int(input("enter the element : "))
-# CAmarks[idx] = inp
-# print(CAmarks) #this will not work as tuples are unchangeable 
+midtmarks = (9,9,8,7,3,1,8)
+finalmarks = CAmarks + midtmarks #concatenate
+print(finalmarks)
+
+print(finalmarks[::-1])
+print(finalmarks[1:])
+print(finalmarks[3:7])
+
+
+#unpacking in tuple
+tuplex = (2,2,5)
+a,b,c=tuplex
+print(c)
+
+print(tuplex.count(2)) #checking the number of times 2 in the tuple
+
+
+tuple1 = (3,2,34,49,30)
+mylist = list(tuple1)
+mylist.remove(34) #element to be removed by typecasting
+tuple1 = tuple(mylist)
+print(tuple1)
