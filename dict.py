0d775e58ad10923f08249211bfe378b5a4ac4a5b
Manish <jangramanish2602@gmail.com>
2024-09-21 02:34:44 +0530

    finding key using loop

--- dict.py
+++ dict.py
@@ -22,3 +22,16 @@ for i in range(1,11):
     sq_value[i]=i**2
 print(sq_value)
 
+#check if key exist
+n = int(input("enter the number for sq root : "))
+
+for key1 in sq_value:
+    #print(key1)
+    if(n==key1):
+        print("found");
+        print(sq_value[key1])
+        break
+else:
+    print("not found")
+    
+        
