84a7ec911fa44c3408f2788f1f545ca12951e94c
Manish <jangramanish2602@gmail.com>
2024-09-30 10:00:00 +0530

    dictionary basic

--- dict.py
+++ dict.py
@@ -5,3 +5,13 @@ dict1 = {
 print(dict1)
 
 print(dict1["color"])
+
+dict1["color"]="Red"
+print(dict1)
+
+for key1 in dict1:
+    #print(key1)
+    print(key1,":",dict[key1])
+
+for values in dict1():
+    print(values)
