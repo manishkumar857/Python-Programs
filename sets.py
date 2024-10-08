4f88ca16f52387224ce97306907c3efb7e543a6a
Manish <jangramanish2602@gmail.com>
2024-10-08 10:40:53 +0530

    add and update in sets

--- sets.py
+++ sets.py
@@ -1,44 +1,57 @@
-camarks = {10,20,30,25,19,29}
-#print(camarks)
-for i in camarks:
-    print(i)
-print(type(camarks))
-#unodered , unchangeable and  not allow duplicates
+# camarks = {10,20,30,25,19,206,29}
+# #print(camarks)
+# for i in camarks:
+#     print(i)
+# print(type(camarks))
+# #unodered , unchangeable and  not allow duplicates
 
-Days=set(["a","b","c","d"])
-print(Days)
-
-
+# Days=set(["a","b","c","d"])
+# print(Days)
 
 
+# #set which have immutable elements
+# set1={1,2,3,"hello,20.5"}
+# print(set1)
+
+#mutable elements like list are not allowed inside sets
+
+#add() add only single item and with update() mutiple elemnts
 
 camarks={'A','O','B','E'}
 print(camarks)
-print(type(camarks))
 
-camarks1={'A',10,"B+",5.8,True}
-print(camarks1)
-#print(camarks1[0]) can't access sets like by index
-print(type(camarks1))
+camarks.add("new")
+print(camarks)
+camarks.update(["new1","new2"])
+print(camarks)
 
-# idx = int(input("enter the number "))
-# camarks[0]=4 #they are unordered we can't change it 
-# print(camarks) 
 
-# camarks1.append(4)
+
+
+
+# camarks1={'A',10,"B+",5.8,True}
 # print(camarks1)
+# #print(camarks1[0]) can't access sets like by index
+# print(type(camarks1))
 
-camarks1.add(7)
-print(camarks1)
-camarks1.pop()
-print(camarks1)
-camarks1.remove(7)
-print(camarks1)
-# camarks1.clear()
+# # idx = int(input("enter the number "))
+# # camarks[0]=4 #they are unordered we can't change it 
+# # print(camarks) 
+
+# # camarks1.append(4)
+# # print(camarks1)
+
+# camarks1.add(7)
 # print(camarks1)
+# camarks1.pop()
+# print(camarks1)
+# camarks1.remove(7)
+# print(camarks1)
+# # camarks1.clear()
+# # print(camarks1)
 
-ca = {1,1,1,1,2}
-print(ca) #not allow duplicates
+# ca = {1,1,1,1,2}
+# print(ca) #not allow duplicates
 
-newset = camarks1.union(ca)
-print(newset)
+# newset = camarks1.union(ca)
+# print(newset)
