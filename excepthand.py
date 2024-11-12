be77dea10f0b1767172370c6b6c484832b4d99eb
Manish <jangramanish2602@gmail.com>
2024-11-12 11:15:45 +0530

    type error

--- excepthand.py
+++ excepthand.py
@@ -9,20 +9,21 @@
 # finally:
 #     print("execution completed")
 
-# try:
-#     x=10
-#     y="python"
-#     print(x+y)
-# except:
-#     print("type error")
-
-
 try:
-    a=int(input("enter first number"))
-    b=int(input("enter second number"))
-    result=a/b
-    print("Result= ",result)
-except ZeroDivisionError:
-    print("zero division error")
-else:
-    print("execution sucessfull")
+    x=int(input("enter number :"))
+    y=input("enter text : ")
+    print(x+y)
+except TypeError:
+    print("type error")
+
+
+# try:
+#     a=int(input("enter first number"))
+#     b=int(input("enter second number"))
+#     result=a/b
+#     print("Result= ",result)
+# except ZeroDivisionError:
+#     print("zero division error")
+# else:
+#     print("execution sucessful")
+
